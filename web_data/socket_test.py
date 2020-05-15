658347f8847d07ebb5b4b26a6dd731144a929673
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-05-15 06:41:21 +0200

    Update socket_test.py

--- web_data/socket_test.py
+++ web_data/socket_test.py
@@ -1,9 +1,9 @@
 import socket
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysocket.connect(('data.pr4e.org',80))
+mysocket.connect(('#link#',80))
 
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = '#protocol_host_doc#'.encode()
 mysocket.send(cmd)
 
 while True:
